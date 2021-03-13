2c08cfa840c874855f2c6e029aa55b399459ba2a
idembele70 <idembele70@gmail.com>
2021-03-13 19:32:52 +0100

    exercice

--- src/main.js
+++ src/main.js
@@ -1,41 +1,48 @@
-// Selector
+/* // selector
 const output = document.getElementById('output');
-// le div du tp3
-const mainDiv = document.createElement('div');
-mainDiv.setAttribute('id', 'divTP3');
-// la paragraphe de presentation
-const paragraph = document.createElement('p');
-paragraph.textContent = "Langages basés sur ECMAScript :";
-mainDiv.appendChild(paragraph)
-const dlElement = document.createElement('dl');
-//les dt et dd
-const dxTabs = [
-    document.createElement('dt'),
-    document.createElement('dd'),
-    document.createElement('dt'),
-    document.createElement('dd'),
-    document.createElement('dt'),
-    document.createElement('dd'),
-    document.createElement('dt'),
-    document.createElement('dd')
-]
-// le contenu des dt et dd
-const dxObj = {
-    1 : "JavaScript",
-    2 : "JavaScript est un langage de programmation de scripts principalement utilisé dans les pages" +
-    "web interactives mais aussi coté serveur.",
-    3 : "JScript",
-    4 : "JScript est le nom générique de plusieurs implémentations d'ECMAScript 3 créées par Microsoft.",
-    5 : "ActionScript",
-    6 : `ActionScript est le langage de programmation utilisé au sein d'applications clientes (Adobe Flash, 
-        Adobe Flex) et serveur (Flash media server, JRun, Macromedia Generator).`,
-    7 : "EX4",
-    8 : 'ECMAScript for XML (E4X) est une extension XML au langage ECMAScript.',
+
+// block
+let divTp1 = document.createElement('div');
+let strongWww = document.createElement('strong');
+let strongW3c = document.createElement('strong');
+let linkOrg = document.createElement('a');
+let linkWww = document.createElement('a');
+
+// 
+const tabs = [document.createTextNode('Le'),document.createTextNode(' World Wide Web Consortium') ,document.createTextNode(', abrégé par le sigle'), document.createTextNode(' W3C')
+, document.createTextNode(' à but non-lucratif chargé de promouvoir la compatibilité des technologies du'), ]
+
+// org link
+linkOrg.href='http://fr.wikipedia.org/wiki/Organisme_de_normalisation';
+linkOrg.title =="Organisme de normalisation";
+linkOrg.textContent = ' organisme de standardisation';
+
+// www link
+linkWww.href="http://fr.wikipedia.org/wiki/World_Wide_Web";
+linkWww.setAttribute('title','World Wide Web')
+linkWww.innerHTML=' World Wide Web.'
+// append element to each other;
+strongWww.appendChild(tabs[1]);
+strongW3c.appendChild(tabs[3]);
+
+output.appendChild(tabs[0]);
+output.appendChild(strongWww);
+output.appendChild(tabs[2]);
+output.appendChild(strongW3c);
+output.appendChild(linkOrg);
+output.appendChild(tabs[4]);
+output.appendChild(linkWww)
+
+// output.appendChild();
+// dark theme
+document.body.style.color = "white";
+document.body.style.backgroundColor = "black"; */
+
+/* ) } */
+
+for (let i = 0; i < 10; i++) {
+    if (i == 5) {
+       continue
+    }
+    console.log(i);
 }
-//ajout du contenu au dt et dd
-for (let i = 0; i < dxTabs.length; i++) {
-    dxTabs[i].innerHTML = dxObj[1];
-    mainDiv.appendChild(dxTabs[i]);
-}
-// ajout du main div au output
-output.appendChild(mainDiv);
