1b88c1f6ae4c03fad39c2a230bdf4fef5cbe002f
idembele70 <idembele70@gmail.com>
2021-04-02 23:50:05 +0200

    inserer des élémets dans une liste

--- src/main.js
+++ src/main.js
@@ -3,5 +3,15 @@ document.body.style.background = "black";
 document.body.style.color = "white";
 
 // function
-const myValue = 1;
-document.body.rem
+
+const zozorBooks = document.querySelector('#zozor-books');
+const li = {
+    0 : document.createElement('li') ,
+    1 : document.createElement('li'),
+}
+li[0].appendChild(document.createTextNode('Zozor à Pôle emploi'));
+zozorBooks.appendChild(li[0]);
+const child4 = zozorBooks.children[5];
+console.log(child4);
+li[1].innerText ="Zozor fait fortune";
+zozorBooks.insertBefore((li[1]),child4)
