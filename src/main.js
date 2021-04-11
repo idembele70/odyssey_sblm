7e659da7096e4c0784dff8e8b8d2c76688e28648
idembele70 <idembele70@gmail.com>
2021-04-11 21:02:47 +0200

    Supprimer les balises BR

--- src/main.js
+++ src/main.js
@@ -2,15 +2,15 @@
 document.body.style.background = "black";
 document.body.style.color = "white";
 
+// Selector
+const output = document.getElementById('output');
+const br = document.querySelectorAll('br')
 // function
+function brremove(elements = br) {
+    for (const element of elements) {
+        element.remove();
+    }
+}
+brremove();
 
-const output = document.querySelector('#output');
-const table = output.querySelector('table');
-const firstTr = document.getElementsByTagName('tr')[2];
-const tr = document.createElement('tr');
-tr.innerHTML =
-    `<th scope="row">Firefox</th>
-    <td>Mozilla</td>
-    <td>27,8%</td> `;
-    console.log(firstTr);
-table.firstChild.insertBefore(tr,firstTr);
+
