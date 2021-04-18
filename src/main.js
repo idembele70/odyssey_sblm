6cfd7129a35184926c33f747f8dce4df4fcd5339
idembele70 <idembele70@gmail.com>
2021-04-18 22:09:11 +0200

    Écrire une fonction de création d'éléments

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 // body style :
-document.body.style.background = "black";
-document.body.style.color = "white";
+// document.body.style.background = "black";
+// document.body.style.color = "white";
 
 // Selector
-var output = document.getElementById('output');
-var child  = output.childNodes[0];
-console.log(child);
-while (child) {
-    output.removeChild(child);
 
-    child = output.firstChild;
+// Function creation d'élements
+
+function createSimpleNode(name,item,child){
+    let elemns =  document.createElement(`${name}`)
+  for (const i in item){
+     elemns.setAttribute(i,item[i])
+  }
+  elemns.innerHTML = child;
+   return elemns
 }
 
+const image = createSimpleNode('img', {src : 'pix.png', alt: 'Image idiote'})
+const link = createSimpleNode('a', {'class' : 'external', href : 'www.siteduzero.com'}, 'Liens vers le Site du Zero')
+
+document.body.firstElementChild.appendChild(image);
+document.body.firstElementChild.innerHTML += `<br/>`
+
+document.body.firstElementChild.appendChild(link);
