1b59e00bfc078be8be125ff60be2beaaf2c2a4ea
idembele70 <idembele70@gmail.com>
2021-04-24 23:13:46 +0200

    supprimer un evenement

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 // body style :
-// document.body.style.background = "black";
-// document.body.style.color = "white";
+document.body.style.background = "black";
+document.body.style.color = "white";
 
 // Selector
 
 // events 
 const output = document.getElementById('output');
-
-const button = output.querySelector('button');
-
-button.onclick = function(){
-   console.log(this.innerHTML = "Vous m'avez cliquer");
+const button = output.children[0];
+function diClick(){
+    alert('Ceci est une alerte')
 }
+const buttonClone = button.cloneNode(true);
+console.log(buttonClone);
+output.appendChild(buttonClone)
+button.addEventListener('click', diClick)
+// button.removeEventListener('click',diClick)
