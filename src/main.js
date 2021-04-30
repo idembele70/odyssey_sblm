3e3ad273c2e766dc10f817359e7cb8c72e530447
idembele70 <idembele70@gmail.com>
2021-04-30 20:44:28 +0200

    Remplissez les champs laissés blancs

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ document.body.style.background = "black";
 document.body.style.color = "white";
 
 // Selector
-const ol = document.querySelector('#output > ol');
-const li = ol.children;
-for (let i = 0; i < li.length; i++) {
-    li[i].addEventListener('click', (e) => {
-        const newValue = prompt("Modifier le text", e.target.textContent)
-        e.target.innerHTML =newValue;
-    })
+const myFramework  = {};
 
+myFramework["checkThisForm"] = function(form) {
+    const inputs = form.querySelectorAll("input");
 }
