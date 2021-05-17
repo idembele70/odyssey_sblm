e42c8e53d847e4f52b5dff68e637b259a9860bc4
idembele70 <idembele70@gmail.com>
2021-05-18 01:25:36 +0200

    Réaliser un toggle

--- src/main.js
+++ src/main.js
@@ -3,13 +3,10 @@ document.body.style.background = "#00000073";
 document.body.style.color = "white";
 
 // Selector
-const td = document.querySelectorAll('td');
+const output = document.querySelector("#output");
 
-td.forEach(el => {
-    el.onclick = _ => el.contentEditable = true;
-    el.addEventListener('keypress', (e) => {
-        if (e.key === 'Enter') {
-            el.contentEditable = false;
-        }
-    })
-})
+const btnToggle = output.getElementsByClassName("btn-toggle")[0];
+const aside = output.getElementsByTagName("aside")[0];
+btnToggle.addEventListener('click' ,()=> {
+    aside.classList.toggle('hide')
+},false);
