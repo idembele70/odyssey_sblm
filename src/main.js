a1ca8de06fc9f6449e003911542b14199a691a1b
idembele70 <idembele70@gmail.com>
2021-05-18 01:49:10 +0200

    toggle

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 // body style :
-document.body.style.background = "#00000073";
 document.body.style.color = "white";
 
 // Selector
@@ -7,6 +6,10 @@ const output = document.querySelector("#output");
 
 const btnToggle = output.getElementsByClassName("btn-toggle")[0];
 const aside = output.getElementsByTagName("aside")[0];
-btnToggle.addEventListener('click' ,()=> {
-    aside.classList.toggle('hide')
-},false);
+btnToggle.addEventListener(
+  "click",
+  () => {
+    aside.classList.toggle("hide");
+  },
+  false
+);
